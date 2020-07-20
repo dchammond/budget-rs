858b97f214c7506e639fc0dc0993918192ebed9f
Dillon Hammond <dillonhammond@gmail.com>
2020-07-19 23:51:51 -0500

    test print transaction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use budget_rs::transaction::Transaction;
+use budget_rs::date::DateInstant;
+use budget_rs::location::Location;
+use budget_rs::currency::Currency;
+use budget_rs::transaction::Category;
+
 fn main() {
-    println!("Hello, world!");
+    let mut t = Transaction::default();
+    t.date(DateInstant::from_date(2020, 7, 19))
+     .vendor("Chipotle")
+     .description("For mom")
+     .location(Location::default().city("Naperville").state("IL").country("USA").clone())
+     .amount(Currency::new(-1560))
+     .category(Category::Restaurants);
+    println!("{}", t);
 }
